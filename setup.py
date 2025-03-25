8cede8fc1da56fa67fc3ab5531be5f365112b557
MMDRZA <info@mmdrza.com>
2025-03-25 21:57:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pyExplorer",
+    name="blockExplorer",
     version="0.1.6",
     author="Mmdrza",
     author_email="pymmdrza@gmail.com",
